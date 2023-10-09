ce0c942ac9ad921e4031dbc0362dfb37e4db8044
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-10-09 17:29:34 +0200

    Update hardhat.config.js

--- EVM-verifier/hardhat.config.js
+++ EVM-verifier/hardhat.config.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.9",
+    version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,

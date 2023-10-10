fef8fb9452983a83d65a6c7ebeef5ad8052f14a2
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-10-10 19:11:43 +0100

    fix: Addressed comments from the PR
    Changed the @openzeppelin/contracts version to 4.x.x because 5.x.x introduces breaking changes.

--- EVM-verifier/contracts/AliceRingToken.sol
+++ EVM-verifier/contracts/AliceRingToken.sol
@@ -40,16 +40,10 @@ contract AliceRingToken is ERC721, ERC721URIStorage, Ownable {
     error OnlyOwnerCanBurn(uint256 tokenId);
 
     RingSignatureVerifier public verifier; // Instance of the ring signature verifier contract
-    uint256 public minimalAmount; // Minimal amount of tokens to be owned by each address in the ring
-
     mapping(string => Status) public mintStatus; // signatureHash => Status (computed off-chain)
 
-    constructor(address initialOwner, address _verifier, uint256 _minimalAmount)
-        ERC721("AliceRingToken", "ART")
-        Ownable(initialOwner)
-    {
+    constructor(address _verifier) ERC721("AliceRingToken", "ART") Ownable() {
         verifier = RingSignatureVerifier(_verifier);
-        minimalAmount = _minimalAmount;
     }
 
     // The following functions are overrides required by Solidity.
@@ -61,43 +55,98 @@ contract AliceRingToken is ERC721, ERC721URIStorage, Ownable {
         return super.supportsInterface(interfaceId);
     }
 
+    /**
+     * @notice safeTransferFrom is disabled because the nft is a sbt
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) public pure override(ERC721, IERC721) {
+        revert("SBT: SafeTransfer not allowed");
+    }
+
+    /**
+     * @notice transferFrom is disabled because the nft is a sbt
+     */
+    function transferFrom(address from, address to, uint256 tokenId) public pure override(ERC721, IERC721) {
+        revert("SBT: Transfer not allowed");
+    }
+
+    /**
+     * @notice approve is disabled because the nft is a sbt and cannot be transferred
+     */
     function approve(address to, uint256 tokenId) public pure override(ERC721, IERC721) {
         revert("SBT: Approve not allowed");
     }
 
+    /**
+     * @notice getApproved is disabled because the nft is a sbt and cannot be transferred
+     */
     function getApproved(uint256 tokenId) public pure override(ERC721, IERC721) returns (address operator) {
         revert("SBT: getApproved not allowed");
     }
 
+    /**
+     * @notice setApprovalForAll is disabled because the nft is a sbt and cannot be transferred
+     */
     function setApprovalForAll(address operator, bool _approved) public pure override(ERC721, IERC721) {
         revert("SBT: setApprovalForAll not allowed");
     }
 
+    /**
+     * @notice isApprovedForAll is disabled because the nft is a sbt and cannot be transferred -> the output will always be false
+     */
     function isApprovedForAll(address owner, address operator) public pure override(ERC721, IERC721) returns (bool) {
         return false;
     }
 
-    function verifyTokenAmounts(address token, Signature memory signature) internal returns (bool) {
+    /**
+     * @notice Verifies the token amounts for each address in the ring
+     *
+     * @param token - the erc20 address of the token we are proving the ownership of
+     * @param signature - the signature object
+     */
+    function verifyTokenAmounts(address token, uint256 minBalance, Signature memory signature)
+        internal
+        returns (bool)
+    {
         // Check that all addresses in the ring own at least the minimal amount of tokens specified
         IERC20 tokenContract = IERC20(token);
         for (uint256 i = 0; i < signature.ring.length; i++) {
             pubKey memory key = signature.ring[i];
             address ringMember = address(uint160(uint256(keccak256(abi.encodePacked(key.x, key.y)))));
-            if (tokenContract.balanceOf(ringMember) < minimalAmount) {
+            if (tokenContract.balanceOf(ringMember) < minBalance) {
                 return false;
             }
         }
         return true;
     }
 
-    function mint(address token, Signature memory signature, string memory uri, string memory proofId) public payable {
+    /**
+     * @notice Mint an SBT
+     *
+     * The sbt is minted to msg.sender
+     * If the verification of the signature is okay see (https://github.com/Cypher-Laboratory/EVM-Verifier/issues/3) and :
+     * - If `mintStatus[proofId] == UNKNOWN`, the proof is minted and the status is set to `MINTED`
+     * - If `mintStatus[proofId] == MINTED`, the proof has already been minted. Tx will revert
+     *
+     * @param token - the erc20 address of the token we are proving the ownership of
+     * @param minBalance - the balance threshold
+     * @param signature - the signature object
+     * @param uri - the IPFS uri
+     * @param proofId - the hash of all the responses used in the proof
+     */
+    function mint(
+        address token,
+        uint256 minBalance,
+        Signature memory signature,
+        string memory uri,
+        string memory proofId
+    ) public payable {
         if (mintStatus[proofId] == Status.MINTED) {
             revert AlreadyMinted(proofId);
         }
         if (!verifier.verify("DummySignature")) {
             revert InvalidSignature();
         }
-        if (!verifyTokenAmounts(token, signature)) {
+        if (!verifyTokenAmounts(token, minBalance, signature)) {
             revert InvalidTokenAmounts();
         }
         mintStatus[proofId] = Status.MINTED;
@@ -106,10 +155,22 @@ contract AliceRingToken is ERC721, ERC721URIStorage, Ownable {
         _nextTokenId++;
     }
 
+    /**
+     * @notice delete all the caracteristics of tokenId (burn)
+     *
+     * Only the owner of an sbt can burn it
+     *
+     * @param tokenId is the id of the sbt to burn
+     */
     function burn(uint256 tokenId) external {
         if (ownerOf(tokenId) != msg.sender) {
             revert OnlyOwnerCanBurn(tokenId);
         }
         _burn(tokenId);
+    }
+
+    // Override the _burn function from ERC721
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
 }
